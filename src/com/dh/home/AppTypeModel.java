3151a0afad88b66f626f45eb094b6c43275d3877
luoran-gaolili <1466746723@qq.com>
2016-07-04 19:49:32 +0800

    add Launcher3
    Signed-off-by: luoran-gaolili <1466746723@qq.com>

---
+++ src/com/dh/home/AppTypeModel.java
@@ -0,0 +1,20 @@
+package com.dh.home;
+
+/**
+ * // v2.1 Created by DemonHunter on 2016/2/2.
+ */
+
+public class AppTypeModel {
+
+    public String appType;
+
+    public String packageName;
+
+    public String className;
+
+    public String title;
+
+    public int itemType;
+
+
+}
